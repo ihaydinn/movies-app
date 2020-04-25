a65ba22ed92771c381f357479da26f4d48c90b75
ihaydinn <ihaydin7@gmail.com>
2020-04-25 18:20:58 +0300

    added splash screen

---
+++ app/src/main/java/com/ismailhakkiaydin/movies/ui/splash/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.ismailhakkiaydin.movies.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.Window
+import android.view.WindowManager
+import com.ismailhakkiaydin.movies.R
+import com.ismailhakkiaydin.movies.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash)
+    }
+
+    override fun onResume() {
+        object : CountDownTimer(3000, 200) {
+
+            override fun onFinish() {
+                var intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+        }.start()
+        super.onResume()
+    }
+}
